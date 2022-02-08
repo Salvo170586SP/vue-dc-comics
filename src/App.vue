430231894b050fc22801f758eb7f4cbb02591b82
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-02-08 15:58:38 +0100

    added Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  min-height: 100vh;
+}
+
+
+.container{
+  margin: 0 auto;
+  max-width: 1200px;
+}
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <figure>
+          <img src="../assets/img/dc-logo.png" alt="DC Logo">
+      </figure>
+      
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+header{
+    min-height: 200px;
+}
+
+
+
+
+</style>
